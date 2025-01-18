3b9ff37fbae875abfd9e33e285f9ccae1ad17972
OGankit505 <mandalankit41@gmail.com>
2025-01-18 22:58:41 +0530

    Dsa Prep

--- src/Recursion/Fibonacci.java
+++ src/Recursion/Fibonacci.java
@@ -9,10 +9,8 @@ public class Fibonacci {
     }
 
     static int fibo(int n){
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
+        if(n == 0 || n == 1)
+            return n;
         return fibo(n-1)+fibo(n-2);
     }
 }
