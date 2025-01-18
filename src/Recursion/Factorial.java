f0c6dcf0477a931c58bd2b3bb3bb269cbd35b4c6
OGankit505 <mandalankit41@gmail.com>
2025-01-18 22:34:54 +0530

    Dsa Prep

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -9,7 +9,7 @@ public class Factorial {
     }
 
     static int numFactorial(int n){
-        if(n == 0 || n==1)
+        if(n == 0)
             return 1;
         return n * numFactorial(n-1);
     }
