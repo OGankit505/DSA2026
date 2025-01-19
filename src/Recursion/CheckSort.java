99bc3e713b8cce9ded7b47d31d898af3c88f4d93
OGankit505 <mandalankit41@gmail.com>
2025-01-19 10:44:12 +0530

    Dsa Prep

--- src/Recursion/CheckSort.java
+++ src/Recursion/CheckSort.java
@@ -5,8 +5,6 @@ public class CheckSort {
         int[] arr = {1, 2, 3, 4, 5};
         int i = 0;
         System.out.println(isSorted(arr, i));
-
-
     }
 
     static boolean isSorted(int[] arr, int i){
