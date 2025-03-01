655dcefc0b20d6c3e77b4ffcf16e0be75b5ce3ba
OGankit505 <mandalankit41@gmail.com>
2025-03-01 23:09:01 +0530

    Dsa Prep

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -13,35 +13,32 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size; // to get the size of the linked list
 
-    //for adding element at the beginning of the linked list
+    // for adding element at the beginning of the linked list
     public void addFirst(int data){
-       //create a new node
-       Node newNode = new Node(data);
+        Node newNode = new Node(data); // create a new node
+        size++;
        //if the linked list is empty
         if(head == null){
             head = tail = newNode;
             return;
         }
-        // point the next of the new node to the head
-        newNode.next = head;
-        //make the new node as head
-        head =  newNode;
+        newNode.next = head; // point the next of the new node to the head
+        head =  newNode; // make the new node as head
     }
 
     //for adding elements at the last of the linked list
     public void addLast(int data){
-        //create a new node
-        Node newNode = new Node(data);
+        Node newNode = new Node(data); //create a new node
+        size++;
         //if the linked list is empty
         if(head == null){
             head = tail = newNode;
             return;
         }
-        // point the next of tail to the new node
-        tail.next = newNode;
-        //make the new node as tail
-        tail = newNode;
+        tail.next = newNode; // point the next of tail to the new node
+        tail = newNode; // make the new node as tail
     }
 
     //for printing the elements of Linked List
@@ -56,7 +53,12 @@ public class LinkedList {
 
     //for adding elements in the middle of the linked list
     public void addMid(int index, int data){
+        if(index == 0){
+            addLast(data);
+            return;
+        }
         Node newNode = new Node(data);
+        size++;
         Node temp =  head;
         int i = 0;
         while(i < index-1){
@@ -75,5 +77,6 @@ public class LinkedList {
         l.addLast(4);
         l.addMid(3,7);
         l.print();
+        System.out.println(l.size);
     }
 }
