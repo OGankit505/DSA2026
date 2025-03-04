a43f8cff3a23aef5ffc3d77585c4383d511232db
OGankit505 <mandalankit41@gmail.com>
2025-03-04 23:02:50 +0530

    Dsa Prep

---
+++ src/PowerOf3Sum_1780.java
@@ -0,0 +1,23 @@
+import java.util.*;
+// problem of the day
+
+public class PowerOf3Sum_1780 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(checkPowersOfThree(n));
+    }
+    static boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n = n/3;
+        }
+        return true;
+
+        // String base3 = Integer.toString(n, 3);
+        //
+        //        return !base3.contains("2");
+    }
+}
