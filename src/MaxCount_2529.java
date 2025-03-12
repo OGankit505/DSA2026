5095f38275cd1ccb0ecbb5b4b68858b34b0c627f
OGankit505 <mandalankit41@gmail.com>
2025-03-12 22:17:47 +0530

    Dsa Prep

---
+++ src/MaxCount_2529.java
@@ -0,0 +1,23 @@
+public class MaxCount_2529 {
+    public static void main(String[] args) {
+        int[] nums = {-2, -1, -1, 1, 2, 3};
+        System.out.println(maximumCount(nums));
+
+    }
+
+    static int maximumCount(int[] nums){
+        int count1=0;
+        int count2=0;
+        for (int num1 : nums) {
+            if (num1 > 0) {
+                count1++;
+            }
+        }
+        for (int num2 : nums) {
+            if (num2 < 0) {
+                count2++;
+            }
+        }
+        return Math.max(count1, count2);
+    }
+}
