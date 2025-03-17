c568cffa39f8152da79448897199f2db10bddab9
OGankit505 <mandalankit41@gmail.com>
2025-03-17 23:53:35 +0530

    Dsa Prep

---
+++ src/EqualPairs_2206.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class EqualPairs_2206 {
+    public static void main(String[] args) {
+        int[] nums = {3,2,3,2,2,2};
+        System.out.println(divideArray(nums));
+
+    }
+    static boolean divideArray(int[] nums){
+        Arrays.sort(nums);
+        int count = 0;
+        for(int i = 1; i<nums.length; i+=2){
+            if(nums[i-1]==nums[i]){
+                count++;
+            }
+        }
+        return count == nums.length / 2;
+    }
+}
